86ea695a1e3509199c6a0e89b88e95a2f3ed09ac
Berrrserrk <tuzov.aleks@list.ru>
2023-02-19 10:07:52 +0300

    Js

---
+++ js/index.js
@@ -0,0 +1,38 @@
+
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>TIME IN MOSCOW:</h1>
+        <h2>{this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Clock />);
+  
